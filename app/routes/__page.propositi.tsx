5f21c69ce17bf62d684b1581ad00f2e398e59b97
Patrick Glendon McCullough <p@mccullo.ug>
2023-05-07 09:51:32 -0400

    Add post-category many-to-many

--- app/routes/__page.propositi.tsx
+++ app/routes/__page.propositi.tsx
@@ -4,40 +4,40 @@ import { useLoaderData } from '@remix-run/react';
 import { Header, NavBar } from '~/components';
 import { V2_MetaFunction } from '@remix-run/node';
 
-export const loader: LoaderFunction = async ({ params }: LoaderArgs) => {
+export const loader: LoaderFunction = async () => {
   const prisma = new PrismaClient();
+  let propositi = {};
   const cfArr:any = [];
-  let userData:any = await prisma.njsoc_posts.findFirst({
-    where: { ID: 209 },
-    include: { custom_fields: true }
-  })
-  userData
-    ?Object.entries(userData).forEach(([key, value]:any) => {
+  let userDataArr:any = await prisma.njsoc_posts.findMany({
+    take: 2,
+    include: { categories: true }
+  }).catch(error => console.log(error));
+  console.log(userDataArr);
+  userDataArr.forEach((userData:any) => {
+    Object.entries(userData).forEach(([key, value]:any) => {
       if(typeof value==="bigint") userData[key] = value.toString();
       if(key==="custom_fields") {
         const customFields = userData[key];
         for(const customField of customFields) cfArr.push([customField.meta_key, customField.meta_value]);
         userData[key] = cfArr;
       }
-    })
-    :""
-  // delete userData.custom_fields;
+    });
+    console.log(userData);
+    propositi = {...propositi, [userData.ID]: userData}
+  })
   await prisma.$disconnect();
-  if(!userData) {
+  if(!propositi) {
     throw new Response(null, {
       status: 404,
       statusText: "Not Found",
     })
   }
-  return userData;
+  console.log(propositi);
+  return propositi;
 }
 
-export const meta: V2_MetaFunction<typeof loader> = ({ data }) => {
-  return (
-    data
-      ?[{ title: `${data.post_title} | The Society of the Cincinnati in the State of New Jersey` }]
-      :[{ title: "The Society of the Cincinnati in the State of New Jersey" }]
-  )
+export const meta: V2_MetaFunction<typeof loader> = () => {
+  return [{ title: "Propositi | The Society of the Cincinnati in the State of New Jersey" }];
 };
 
 export default function Page() {
@@ -47,9 +47,10 @@ export default function Page() {
       <Header />
       <NavBar />
       <main className="content">
-        <h1>{userData?.post_title}</h1>
+        {/* {Object.entries(userData)?.map(prop => <div>{JSON.stringify(prop)}</div>)} */}
+        {/* <h1>{userData?.post_title}</h1>
         <article dangerouslySetInnerHTML={{__html: userData?.post_content}} />
-        {userData.custom_fields.map(cf => <div key={`${cf[0]}${cf[1]}`}>{cf[0]}: {cf[1]}</div>)}
+        {userData.custom_fields.map(cf => <div key={`${cf[0]}${cf[1]}`}>{cf[0]}: {cf[1]}</div>)} */}
       </main>
     </>
   )
