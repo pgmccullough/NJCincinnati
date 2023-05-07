361ef262f034aa9bdbb082fd88b14db9f7c10ad7
Patrick Glendon McCullough <p@mccullo.ug>
2023-05-07 12:57:03 -0400

    Get posts by category

--- app/routes/__page.propositi.tsx
+++ app/routes/__page.propositi.tsx
@@ -6,25 +6,45 @@ import { V2_MetaFunction } from '@remix-run/node';
 
 export const loader: LoaderFunction = async () => {
   const prisma = new PrismaClient();
-  let propositi = {};
-  const cfArr:any = [];
-  let userDataArr:any = await prisma.njsoc_posts.findMany({
-    take: 2,
-    include: { categories: true }
-  }).catch(error => console.log(error));
-  console.log(userDataArr);
-  userDataArr.forEach((userData:any) => {
-    Object.entries(userData).forEach(([key, value]:any) => {
-      if(typeof value==="bigint") userData[key] = value.toString();
+
+  let catId:any = await prisma.njsoc_terms.findFirst({
+    where: { name: 'Propositi' }
+  })
+  .catch(error => console.log(error));
+
+  let matchData:any = await prisma.njsoc_term_taxonomy.findFirst({
+    where: { term_id: catId.term_id },
+    include: { 
+      posts: { 
+        include: { 
+          post: {
+            include: {
+              custom_fields: true
+            }
+          }
+        }
+      } 
+    }
+  })
+  .catch(error => console.log(error));
+
+  const { posts } = matchData;
+
+  const propositi = posts.map((post:any) => post.post);
+
+  // DEAL WITH BIGINT BUG
+  for( let propositus of propositi ) {
+    Object.entries(propositus).forEach(([key, value]:any) => {
+      if(typeof value==="bigint") propositus[key] = value.toString();
       if(key==="custom_fields") {
-        const customFields = userData[key];
-        for(const customField of customFields) cfArr.push([customField.meta_key, customField.meta_value]);
-        userData[key] = cfArr;
+        const customFields = propositus[key];
+        const cf = [];
+        for(const customField of customFields) cf.push({[customField.meta_key]: customField.meta_value});
+        propositus[key] = JSON.stringify(cf);
       }
     });
-    console.log(userData);
-    propositi = {...propositi, [userData.ID]: userData}
-  })
+  }
+
   await prisma.$disconnect();
   if(!propositi) {
     throw new Response(null, {
@@ -32,7 +52,6 @@ export const loader: LoaderFunction = async () => {
       statusText: "Not Found",
     })
   }
-  console.log(propositi);
   return propositi;
 }
 
@@ -40,17 +59,33 @@ export const meta: V2_MetaFunction<typeof loader> = () => {
   return [{ title: "Propositi | The Society of the Cincinnati in the State of New Jersey" }];
 };
 
+const getCustom = (fields:{[key: string]: string}[], key:string) => {
+  const field = fields.find((field:any) => field[key]);
+  return field?field[key]:"";
+}
+
 export default function Page() {
-  let userData: any = useLoaderData<any>();
+  let propositi: any = useLoaderData<any>();
+  propositi = propositi.map((propositus:any) => {
+    return {...propositus, custom_fields: JSON.parse(propositus.custom_fields)}
+  })
+
+  propositi.sort((a:any,b:any) => {
+    const aSort = getCustom(a.custom_fields, "Sort Last Name");
+    const bSort = getCustom(b.custom_fields, "Sort Last Name");
+    return aSort.localeCompare(bSort);
+  })
+
   return (
     <>
       <Header />
       <NavBar />
       <main className="content">
-        {/* {Object.entries(userData)?.map(prop => <div>{JSON.stringify(prop)}</div>)} */}
-        {/* <h1>{userData?.post_title}</h1>
-        <article dangerouslySetInnerHTML={{__html: userData?.post_content}} />
-        {userData.custom_fields.map(cf => <div key={`${cf[0]}${cf[1]}`}>{cf[0]}: {cf[1]}</div>)} */}
+        {propositi.map((propositus: any, i: number) => {
+          return (
+            <div key={propositus.ID}>{getCustom(propositus.custom_fields,"Sort Last Name")}</div>
+          )
+        })}
       </main>
     </>
   )
